4df6f5ea119f9701eac09561995eda0be99e59c1
asdfrewqha <asdfrewqha@gmail.com>
2025-03-10 09:40:55 +0300

    minor

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ app.include_router(auth.router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Разрешить запросы с любого источника
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
